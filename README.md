558b55c5ab5dd1fd34dfada09f6388598794a82b
Kirell Benzi <kirell.benzi@epfl.ch>
2017-01-13 17:58:39 +0100

    More documentation

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 This repo contains boilerplate code for a d3 viz.
 
+First, install the packages:
+
+```bash
+$ npm install
+```
+
+To start developing run:
+
 ```bash
 $ npm start
 ```
 
-You should see a new browser tap opening and a title of "Hello D3" in http://127.0.0.1:8080.
+You should see a new browser tap opening and a title of "D3 Boilerplate" in http://127.0.0.1:8080.
 
 From there, you start to develop your own code in the `app` directory. When you finish coding, use `npm run build` or `npm run deploy` to build the static files.
 
--- app/main.js
+++ app/main.js
@@ -1,22 +1,13 @@
-// Starting point
 'use strict';
 
 require("./main.scss");  // will build CSS from SASS 3 file
 
-// import _ from 'lodash';
+import _ from 'lodash';  // imports
 import * as d3 from 'd3';
 
 // Test ES6 and d3
 const data = [1, 2, 3];
-const [x, y] = [70, 10];
-
-console.log(data);
-let helpers = {
-  translate: function(x, y) {
-    this.attr("transform", `translate(${x}, ${y})`);
-    return this;
-  }
-}
+const [x, y] = [70, 100];  // Array destructuring
 
 let svg = d3.select("#viz")
             .append("svg")
@@ -24,10 +15,7 @@ let svg = d3.select("#viz")
             .attr("height", 500);
 
 let rec = svg.append("rect")
-  .attr("id", "myrect")
+  .attr("id", "myrect")  // Rect color is given by a compiled SCSS file that includes another SCSS file /style/colors.scss
   .attr("width", 200)
   .attr("height", 150)
-
-// let's create a transform method that can be chained
-rec = Object.assign(rec, helpers);
-rec.translate(x, y);
+  .attr("transform", `translate(${x}, ${y})`);  // string interpolation
